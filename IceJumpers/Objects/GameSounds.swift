4538da8201f356236648dff0aee247000aa207d7
Francisco Diarte <fdiarte95@gmail.com>
2018-05-25 10:36:39 -0600

    add power up sounds

--- IceJumpers/Objects/GameSounds.swift
+++ IceJumpers/Objects/GameSounds.swift
@@ -13,6 +13,11 @@ class GameSounds {
     
     static let shared = GameSounds()
     private var audioPlayer = AVAudioPlayer()
+    private var powerUpPlayer = AVAudioPlayer()
+    
+    func stopPlayingSound() {
+        audioPlayer.stop()
+    }
     
     func playMenuSound() {
         guard let url = Bundle.main.url(forResource: "Menu", withExtension: "mp3") else { return }
@@ -26,13 +31,10 @@ class GameSounds {
         }
     }
     
-    func stopPlayingSound() {
-        audioPlayer.stop()
-    }
-    
     func playInGameSounds() {
         guard let url = Bundle.main.url(forResource: "InGameSounds", withExtension: "mp3") else { return }
         do {
+            audioPlayer.stop()
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
             try AVAudioSession.sharedInstance().setActive(true)
             audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
@@ -45,25 +47,53 @@ class GameSounds {
     func playDeathSound() {
         guard let url = Bundle.main.url(forResource: "DeathSound", withExtension: "mp3") else { return }
         do {
+            audioPlayer.stop()
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
             try AVAudioSession.sharedInstance().setActive(true)
             audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
             audioPlayer.play()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.playInGameSounds()
+            }
         } catch let error {
             print(error.localizedDescription)
         }
     }
     
-//    func playPowerUpSound() {
-//        guard let url = Bundle.main.url(forResource: "PowerUpSound", withExtension: "mp3") else { return }
-//        do {
-//            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-//            try AVAudioSession.sharedInstance().setActive(true)
-//            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-//            audioPlayer.play()
-//        } catch let error {
-//            print(error.localizedDescription)
-//        }
-//    }
+    func playPowerUpSoundOne() {
+        guard let url = Bundle.main.url(forResource: "powerUpOne", withExtension: "wav") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            powerUpPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            powerUpPlayer.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func playPowerUpSoundTwo() {
+        guard let url = Bundle.main.url(forResource: "powerUpTwo", withExtension: "wav") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            powerUpPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            powerUpPlayer.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func playPowerUpSoundThree() {
+        guard let url = Bundle.main.url(forResource: "powerUpThree", withExtension: "wav") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            powerUpPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            powerUpPlayer.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
